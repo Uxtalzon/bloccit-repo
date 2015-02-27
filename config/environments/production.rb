a9eeaff35ae48f52cde7c36df784f06dbdee8e0b
Phil <uxtalzon@yahoo.com>
2015-02-27 18:46:58 +0000

    Yet another fix for productions.rb URL

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,5 +82,5 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   
   # httP://bloccit.herokuapp.com
-  config.action_mailer.default_url_options = { host: 'uxtalzon-bloccit.herokuapp.com'' }
+  config.action_mailer.default_url_options = { host: 'uxtalzon-bloccit.herokuapp.com }
 end
