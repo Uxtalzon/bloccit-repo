7a9f98efed3af3093022609a5c4254c1d067fc51
Phil <uxtalzon@yahoo.com>
2015-03-04 15:57:12 +0000

    Really complete Post Markdowns checkpoint

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,10 +14,11 @@ class PostsController < ApplicationController
   def create
      @post = current_user.posts.build(post_params)
      @topic = Topic.find(params[:topic_id])
+    @post.topic = @topic
      authorize @post
    if @post.save
        flash[:notice] = "Post was saved."
-       redirect_to @post
+       redirect_to [@topic, @post]
    else
        flash[:error] = "There was an error saving the post. Please try again."
        render :new
@@ -32,12 +33,12 @@ class PostsController < ApplicationController
   end
   
   def update
-    @post = current_user.posts.build(post_params)
+    @post = Post.find(params[:id])
     @topic = Topic.find(params[:topic_id])
     authorize @post
      if @post.update_attributes(params.require(:post).permit(:title, :body))
        flash[:notice] = "Post was updated."
-       redirect_to @post
+       redirect_to [@topic, @post]
      else
        flash[:error] = "There was an error saving the post. Please try again."
        render :edit
