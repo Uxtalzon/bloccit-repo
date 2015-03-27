b10c8c225cf231ddd0f8b9217e19b991c1b6761c
Phil <uxtalzon@yahoo.com>
2015-03-27 15:55:23 +0000

    Voting checkpoint

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,9 @@ Rails.application.routes.draw do
   resources :topics do
     resources :posts, except: [:index] do
       resources :comments, only: [:create, :destroy]
+      
+      post '/up_vote' => 'votes#up_vote', as: :up_vote
+      post '/down_vote' => 'votes#down_vote', as: :down_vote
     end
   end
 
